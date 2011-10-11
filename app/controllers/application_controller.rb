5b705e8d09b42c2a6f03574aaab24cd45d6e7c36
Sporas Laszlo <lackuszzz@freemail.hu>
2011-10-11 18:43:47 +0200

    Done with static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def home
+    @title = "Home"
+  end
+
+  def contact
+    @title = "Contact"
+  end
+
+  def about
+    @title = "About"
+  end
 end
